d538d27fb195d802a21aa7ef6f34f23c2b7507c2
samuel <129302031+samuelfrencia@users.noreply.github.com>
2023-08-15 02:10:35 -0300

    intentado mostrar las compras

--- src/views/MisCryptoView.vue
+++ src/views/MisCryptoView.vue
@@ -6,22 +6,29 @@
     <div class="muestraDatos">
       <div class="row">
         <div class="col-md-1"></div>
-        <div class=" col-md-10">
+        <div class="col-md-10">
           <h2 style="text-align: center;">Mi billetera</h2>
           <table>
-            <tr>
-              <th>Crypto</th>
-              <th>Cantidad</th>
-              <th>Precio compra</th>
-              <th>Ganancias</th>
-            </tr>
-            <tr v-for="(cripto, index) in criptos" :key="index">
-              <td >{{ cripto.nombre }}</td>
-              <td>{{ cripto.cantidad }}</td>
-              <td>${{ cripto.precioCompra }}</td>
-              <td>${{ cripto.ganancia }}</td>
-            </tr>
-
+            <thead>
+              <tr>
+                <th>Crypto</th>
+                <th>Cantidad</th>
+                <th>Fecha</th>
+                <th>Precio compra</th>
+                <th>Ganancias</th>
+                <th>Buttons</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(cripto, index) in criptos" :key="index">
+                <td>{{ cripto.crypto_code }}</td>
+                <td>{{ cripto.crypto_amount }}</td>
+                <td>{{ cripto.datetime }}</td>
+                <td>${{ cripto.money }}</td>
+                <td>${{ cripto.ganancia }}</td>
+                <td>Botones</td>
+              </tr>
+            </tbody>
           </table>
         </div>
         <div class="col-md-1"></div>
@@ -39,40 +46,43 @@ import NavbarView from '@/components/navbarView.vue';
 import FooterView from '@/components/footerView.vue';
 
 export default {
-    data() {
-        return {
-            criptos: [
-                { nombre: "BITCOIN", cantidad: 5, precioCompra: 24, ganancia: 51 },
-                { nombre: "ETHEREUM", cantidad: 2, precioCompra: 20, ganancia: 41 },
-                { nombre: "USDT", cantidad: 3, precioCompra: 15, ganancia: 1 },
-                { nombre: "DAI", cantidad: 6, precioCompra: 14, ganancia: 5 },
-            ],
-            compras: [],
-        };
-    },
-    created() {
-        this.fetchCompras();
-    },
-    methods: {
-        fetchCompras() {
-            // Realiza una solicitud GET a tu API para obtener los datos de las compras realizadas
-            axios.get("https://laboratorio3-f36a.restdb.io/rest/compras")
-                .then((response) => {
-                this.compras = response.data;
-            })
-                .catch((error) => {
-                console.error("Error al obtener las compras:", error);
-            });
+  data() {
+    return {
+      criptos: [],
+      usuario: ''
+    };
+  },
+  created() {
+    this.usuario = JSON.parse(localStorage.getItem('user'))
+  },
+  mounted() {
+    this.fetchData(); // Llama al método para obtener los datos al cargar el componente
+  },
+  methods: {
+    fetchData() {
+      axios.get('https://laboratorio3-f36a.restdb.io/rest/transactions', {
+        headers: {
+          'Content-type': 'application/data',
+          'APIKEY': '60eb09146661365596af552f'
         },
-    },
-    components: { NavbarView,
-                  FooterView
-                }
+      })
+        .then(response => {
+          console.log('Respuesta de la API:', response.data);
+          
+          this.criptos = response.data; // Almacena los datos en el arreglo criptos
+        })
+        .catch(error => {
+          console.error('Error al obtener los datos:', error);
+        });
+    }
+  },
+  components: {
+    NavbarView,
+    FooterView
+  }
 };
 </script>
   
 <style>
 /* Estilos del archivo style.css */
 </style>
-  
-  
