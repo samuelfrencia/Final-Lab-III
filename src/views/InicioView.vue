7277ea1ad1d3266c2e07e302487830e8bf3090ac
samuel <129302031+samuelfrencia@users.noreply.github.com>
2023-08-24 02:45:56 -0300

    .

--- src/views/InicioView.vue
+++ src/views/InicioView.vue
@@ -28,7 +28,7 @@
               </thead>
               <tbody>
                 <tr v-for="(dato, index) in datos" :key="index">
-                  <td>{{ criptos[index] }}</td>
+                  <td>{{ monedas[index] }}</td>
                   <td>{{ '$' + dato.ask }}</td>
                   <td>{{ '$' + dato.totalAsk }}</td>
                   <td>{{ '$' + dato.totalBid }}</td>
@@ -47,12 +47,13 @@
 <script>
 import NavbarView from '@/components/navbarView.vue';
 import FooterView from '@/components/footerView.vue';
+import axios from 'axios';
 export default {
   data() {
     return {
       isBalanceVisible: true,
       totalMoney: 0,
-      criptos: [
+      monedas: [
         "BITCOIN",
         "ETHEREUM",
         "USDT",
@@ -78,6 +79,8 @@ export default {
     };
   },
   mounted() {
+    this.usuario = JSON.parse(localStorage.getItem('user'))
+    this.traerTransacciones();
     this.obtenerDatos();
     const totalMoney = parseFloat(localStorage.getItem('totalMoney'));
     this.totalMoney = totalMoney;
@@ -129,7 +132,48 @@ export default {
             this.tabla(apiResult, index);
           });
         });
-    }
+    },
+    traerTransacciones() {
+      axios.get(`https://laboratorio-36cf.restdb.io/rest/transactions?q={"user_id":"${this.usuario}"}`, {
+        headers: {
+          'x-apikey': '64a5ccf686d8c5d256ed8fce'
+        },
+      })
+        .then(response => {
+          console.log('Respuesta de la API:', response.data);
+
+          this.criptos = response.data.map(transaccion => {
+            const fechaUTC = new Date(transaccion.datetime);
+            const offset = 3; // Diferencia horaria en horas para Buenos Aires
+            const fechaBuenosAires = new Date(fechaUTC.getTime() + offset * 60 * 60 * 1000);
+
+            const dia = fechaBuenosAires.getDate().toString().padStart(2, '0');
+            const mes = (fechaBuenosAires.getMonth() + 1).toString().padStart(2, '0');
+            const anio = fechaBuenosAires.getFullYear();
+            const horas = fechaBuenosAires.getHours().toString().padStart(2, '0');
+            const minutos = fechaBuenosAires.getMinutes().toString().padStart(2, '0');
+
+            transaccion.datetimeFormatted = `${dia}/${mes}/${anio}, ${horas}:${minutos}`;
+
+            return transaccion;
+
+          });
+          // Calcular la suma de dinero y asegurarse de que no tenga decimales
+          const totalMoney = this.criptos.reduce((total, transaccion) => total + parseFloat(transaccion.money), 0);
+
+          // Redondear hacia abajo para obtener un número entero
+          const totalMoneySinDecimales = Math.floor(totalMoney);
+
+          console.log('Total de dinero:', totalMoneySinDecimales);
+
+          // Guardar el totalMoney en localStorage
+          localStorage.setItem('totalMoney', totalMoneySinDecimales);
+          
+        })
+        .catch(error => {
+          console.error('Error al obtener los datos:', error);
+        });
+    },
   },
   components: {
     NavbarView,
