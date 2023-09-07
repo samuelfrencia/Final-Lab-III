6781db61106c9e559edb848abc566fee381f1690
samuel <129302031+samuelfrencia@users.noreply.github.com>
2023-09-07 00:06:09 -0300

    cambios

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,7 +4,8 @@
     <navbarViewLogin></navbarViewLogin>
     <br><br><br>
     <!--VALIDACION-->
-    <div class="alert alert-danger d-flex align-items-center container" role="alert" v-if="correcto == false" id="alertWarning">
+    <div class="alert alert-danger d-flex align-items-center container" role="alert" v-if="correcto == false"
+      id="alertWarning">
       <div>
         ERROR: El usuario debe contener al menos una mayúscula, una minúscula y un número
       </div>
@@ -17,15 +18,25 @@
         </div>
         <div class="card-body">
           <form @submit.prevent="submitForm" id="loginForm">
-            <label for="usuario" class="usuario">Usuario:</label>
-            <input type="text" v-model="usuario" id="usuario" required />
+            <input class="input-group-text" type="text" v-model="usuario" id="usuario" required placeholder="Ingrese usuario" autocomplete="off" style="width: 100%; box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);"/>
+            <div class="content">
+              <p>El usuario debe contener:</p>
+              <ul class="requirement-list">
+                <li v-for="(requirement, index) in requirements" :key="index"
+                  :class="{ valid: isRequirementValid(requirement.regex) }" 
+                  style=""
+                  >
+                  <span>{{ requirement.label }}</span>
+                </li>
+              </ul>
+            </div>
             <button type="submit" id="loginButton">Ingresar</button>
           </form>
         </div>
       </div>
     </div>
 
-    <br><br><br><br>
+    <br><br>
     <!--FOOTER-->
     <footerViewLogin></footerViewLogin>
   </div>
@@ -39,9 +50,19 @@ export default {
     return {
       usuario: '',
       correcto: true,
+      showusuario: false,
+      requirements: [
+        { regex: /[0-9]/, label: 'Un numero (0...9)' },
+        { regex: /[a-z]/, label: 'Letra en minúscula (a...z)' },
+        { regex: /[A-Z]/, label: 'Letra en mayúscula (A...Z)'  },
+      ],
     };
   },
   methods: {
+
+    isRequirementValid(regex) {
+      return regex.test(this.usuario);
+    },
     submitForm() {
       const validacion = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+$/;
       if (validacion.test(this.usuario)) {
@@ -61,9 +82,51 @@ export default {
 </script>
   
 <style scoped>
-#alertWarning{
+#alertWarning {
   display: flex;
   justify-content: center;
   width: 50%;
 }
-</style>
+/* Import Google font - Poppins */
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
+
+.content {
+  margin: 20px 0 10px;
+}
+.content p {
+  color: black;
+  font-size: 1.2rem;
+}
+.requirement-list li {
+  font-size:1rem;
+  list-style: none;
+  display: flex;
+  align-items: left;
+}
+
+.requirement-list li i {
+  width: 20px;
+  color: #aaa;
+  font-size: 0.6rem;
+}
+
+.requirement-list li span {
+  margin-left: 12px;
+  color: black;
+}
+
+.requirement-list li.valid span {
+  color: darkgrey;
+}
+
+@media screen and (max-width: 500px) {
+  .content p {
+    font-size: 1.15rem;
+  }
+  .requirement-list li {
+    font-size: 1.1rem;
+  }
+  .requirement-list li span {
+    margin-left: 7px;
+  }
+}</style>
